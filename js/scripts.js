1d38603f315b9696f009dcecdcd2047e27df5af1
John Powell <powell.john2011@gmail.com>
2022-03-06 16:18:35 +0300

    adding initial files

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+const add = (value1, value2) => value1 + value2;
+const vnumber1 = parseInt(prompt("Enter first value to add"));
+const vnumber2 = parseInt(prompt("Enter second value to add"));
+
+alert(add(vnumber1, vnumber2));
